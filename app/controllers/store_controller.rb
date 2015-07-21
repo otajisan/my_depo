c8da17f63cd361cc25dd138bf25ca8774a06f31c
otajisan <taji.taji.otajisan@gmail.com>
2015-07-21 09:56:30 +0900

    第9章

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,6 +1,12 @@
 class StoreController < ApplicationController
   def index
     @products = Product.order(:title)
+    # 画面表示時にカウンタを加算
+    if session[:counter].nil?
+        session[:counter] = 1
+    else
+        session[:counter] += 1
+    end
   end
 end
 
